3f08d8e12e7005516d22cc486118956e99a9d9e5
Tomas Makaj <Tmakajcoding@gmail.com>
2022-11-27 20:45:08 -0500

    Architects

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/navbar/Navbar';
 import Card from './components/card/Card'
 import Mission from './components/mission/Mission'
 import Partner from './components/partner/Partner';
+import Architects from './components/architects/Architects';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Mission />
       <Card />
       <Partner />
+      <Architects />
     </div>
   );
 }
---
+++ src/components/architects/Architects.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Architects.css'
+
+
+const Architects = () => {
+  return (
+    <div>
+        <h1>Overlook of our Architects</h1>
+        <div className='architects'>
+        <a href="https://www.lamarca-butler.com/" target="_blank">LAMARCA | BUTLER GROUP</a>
+        <a href="https://www.janekimarchitect.com/" target="_blank">JANE KIM DESIGN</a>
+        <a href="https://kohnarchitecture.nyc/#one" target="_blank">KOHN ARCHITECTURE</a>
+        <a href="https://www.studiolyonszot.com/" target="_blank">STUDIO LYON SZOT</a>
+        <a href="https://www.dcapny.com/" target="_blank">DCAP</a>
+        </div>
+
+    </div>
+  )
+}
+
+export default Architects
